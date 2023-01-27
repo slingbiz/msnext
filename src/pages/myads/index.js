8380e44550e5274707120d123296012060c8efeb
Ankur Pata <ankur.patankar@timedoctor.com>
2023-01-27 10:44:17 +0400

    with Auth MS

--- src/pages/myads/index.js
+++ src/pages/myads/index.js
@@ -220,9 +220,11 @@ const MyAdsPage = ({pageComponentProps}) => {
 
 export async function getServerSideProps(ctx) {
   const {req, res} = ctx;
+
   const response = await axios.get('https://www.motorsingh.com/user/validate', {
     headers: {cookie: `PHPSESSID=${req.cookies.PHPSESSID};`}
   });
+
   if (!response?.data?.user_id) {
     return {
       redirect: {
