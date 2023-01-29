ca6552fce34d025916fa455a00ed7ca6db56ea66
Ankur Pata <ankur.patankar@timedoctor.com>
2023-01-29 15:08:18 +0400

    route with auth fo rapiS

--- src/constants/common.js
+++ src/constants/common.js
@@ -1,3 +1,3 @@
-export const SERVICE_URL = 'http://motorsingh.com:2023/v2';
+export const SERVICE_URL = 'https://www.motorsingh.com/next/v2';
 
 export const SOMETHING_WENT_WRONG = 'Something went wrong';
