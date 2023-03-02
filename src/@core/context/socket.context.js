d98c9c2067df81408db2efd35a10eb1f08b0f504
Ankur Pata <ankur.patankar@timedoctor.com>
2023-03-03 00:20:09 +0400

    socket.io path sub

---
+++ src/@core/context/socket.context.js
@@ -0,0 +1,55 @@
+import { createContext, useContext, useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import io from 'socket.io-client'
+import EVENTS from 'src/constants/events'
+import { SOCKET_URL, SOCKET_URL_SUB_PATH } from '../../constants/common'
+
+const socket = io(SOCKET_URL, { path: SOCKET_URL_SUB_PATH })
+
+const SocketContext = createContext({ socket, conUsers: [], messages: [], notif: [] })
+
+function SocketsProvider(props) {
+  const [conUsers, setConUsers] = useState([])
+  const [messages, setMessages] = useState([])
+  const [notif, setNotif] = useState([])
+
+  const loggedUser = useSelector(({ myAccount }) => myAccount.singleUser)
+
+  useEffect(() => {
+    if (loggedUser.length > 0) {
+      const userId = loggedUser[0].user_id
+      socket.emit(EVENTS.CLIENT.REGISTER, userId)
+    }
+  }, [loggedUser])
+
+  socket.on(EVENTS.SERVER.NOTIFY, data => {
+    const { message, receiver } = data
+    const truncMsg = `${message.slice(0, 5)}...`
+
+    setNotif([...notif, { truncMsg, receiver }])
+  })
+
+  socket.on(EVENTS.SERVER.USERS, users => {
+    setConUsers(users)
+  })
+
+  socket.on(EVENTS.CLIENT.PRIVATE_CHAT, data => {
+    const { sender, message, car, receiver } = data
+
+    setMessages([...messages, { sender, message, car, receiver }])
+  })
+
+  socket.on(EVENTS.SERVER.ERROR, e => {
+    console.log(e.error)
+  })
+
+  socket.onAny((event, ...args) => {
+    console.log('Event called: ', event, args)
+  })
+
+  return <SocketContext.Provider value={{ socket, messages, setMessages, conUsers, notif, setNotif }} {...props} />
+}
+
+export const useSockets = () => useContext(SocketContext)
+
+export default SocketsProvider
--- src/constants/common.js
+++ src/constants/common.js
@@ -1,3 +1,7 @@
-
 export const SERVICE_URL = process.env.NEXT_PUBLIC_SERVICE_URL
+
+export const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL
+
+export const SOCKET_URL_SUB_PATH = process.env.NEXT_PUBLIC_SOCKET_URL_SUB_PATH
+
 export const SOMETHING_WENT_WRONG = 'Something went wrong'
