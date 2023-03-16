3d7ed6fbbc0586a93da73b6585048cf7a19ebcf8
Ankur Pata <ankur.patankar@timedoctor.com>
2023-03-16 17:58:23 +0400

    remove hardcoded toekns

--- src/pages/mycars/index.js
+++ src/pages/mycars/index.js
@@ -196,9 +196,9 @@ export async function getServerSideProps(ctx) {
   const { req, res } = ctx
 
   const response = await axios.get('https://www.motorsingh.com/user/validate', {
-    // headers: { cookie: `PHPSESSID=${req.cookies.PHPSESSID};` }
+    headers: { cookie: `PHPSESSID=${req.cookies.PHPSESSID};` }
 
-    headers: { cookie: `PHPSESSID=7e952iigfbbkvle1v0j61tn8c3` }
+    // headers: { cookie: `PHPSESSID=7e952iigfbbkvle1v0j61tn8c3` }
   })
 
   if (!response?.data?.user_id) {
